6e6c97c6bf84221d831a7904aa3c83468c072647
mogyoro <crynion@gmail.com>
2017-04-28 02:56:07 -0700

    Add tests and jacoco

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,80 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
-
+  private TorpedoStore mockPrimaryTS;
+  private TorpedoStore mockSecondaryTS;
   @Before
   public void init(){
-    this.ship = new GT4500();
+	mockPrimaryTS = mock(TorpedoStore.class);
+	mockSecondaryTS = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPrimaryTS, mockSecondaryTS);
   }
 
   @Test
   public void fireTorpedos_Single_Success(){
     // Arrange
+	when(mockPrimaryTS.isEmpty()).thenReturn(false);
+	when(mockPrimaryTS.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedos(FiringMode.SINGLE);
 
     // Assert
+    verify(mockPrimaryTS, times(1)).isEmpty();
+    verify(mockPrimaryTS, times(1)).fire(1);
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedos_All_Success(){
     // Arrange
-
+	when(mockPrimaryTS.isEmpty()).thenReturn(false);
+	when(mockSecondaryTS.isEmpty()).thenReturn(false);
+	when(mockPrimaryTS.fire(1)).thenReturn(true);
+	when(mockSecondaryTS.fire(1)).thenReturn(true);
+	
     // Act
     boolean result = ship.fireTorpedos(FiringMode.ALL);
 
     // Assert
+    verify(mockPrimaryTS, times(1)).isEmpty();
+    verify(mockPrimaryTS, times(1)).fire(1);
+    verify(mockSecondaryTS, times(1)).isEmpty();
+    verify(mockSecondaryTS, times(1)).fire(1);
     assertEquals(true, result);
   }
+  
+  @Test
+  public void fireTorpedos_Single_PrimaryFirst_Success() {
+	  //Arrange
+	  when(mockPrimaryTS.fire(1)).thenReturn(true);
+	  when(mockPrimaryTS.isEmpty()).thenReturn(false);
+	  
+	  //Act
+	  boolean result = ship.fireTorpedos(FiringMode.SINGLE);
+	  
+	  //Assert
+	  verify(mockPrimaryTS, times(1)).fire(1);
+	  verify(mockSecondaryTS, never()).fire(anyInt());
+	  assertEquals(true, result);
+  }
+  
+  @Test
+  public void fireTorpedos_Single_TwoTimes_Alternating_Success() {
+	  //Arrange
+	  when(mockPrimaryTS.isEmpty()).thenReturn(false);
+	  when(mockSecondaryTS.isEmpty()).thenReturn(false);
+	  when(mockPrimaryTS.fire(1)).thenReturn(true);
+	  when(mockSecondaryTS.fire(1)).thenReturn(true);
+	  
+	  //Act
+	  boolean result = ship.fireTorpedos(FiringMode.SINGLE) && ship.fireTorpedos(FiringMode.SINGLE);
+	  
+	  //Assert
+	  verify(mockPrimaryTS, times(1)).fire(1);
+	  verify(mockSecondaryTS, times(1)).fire(1);
+	  assertEquals(true, result);
+  }
+  
 
 }
